61d7ec80215635a4678f771a87defd33144c68ea
candy-6646 <59140242+candy-6646@users.noreply.github.com>
2020-11-05 06:10:28 -0500

    Create respose.php

---
+++ respose.php
@@ -0,0 +1,6 @@
+$host        = "host = ec2-52-73-199-211.compute-1.amazonaws.com";
+   $port        = "port = 5432";
+   $dbname      = "dbname = d8h0u8kh19dh36";
+   $credentials = "user = qownjuhbawznbz password=368dd7b964487933e40c9d030fd89399ae054c69a15b3f210ad7eea2917bebde";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
