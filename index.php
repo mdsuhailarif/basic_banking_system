159c927e6ec7c3f96dd3e36276c074edfd1d09a5
candy-6646 <59140242+candy-6646@users.noreply.github.com>
2020-11-05 01:45:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
+$host        = "host = ec2-52-73-199-211.compute-1.amazonaws.com";
+   $port        = "port = 5432";
+   $dbname      = "dbname = d8h0u8kh19dh36";
+   $credentials = "user = qownjuhbawznbz password=368dd7b964487933e40c9d030fd89399ae054c69a15b3f210ad7eea2917bebd";
 
-include_once("homepage.html");
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+
+$sql =<<<EOF
+   CREATE TABLE IF NOT EXISTS customers
+   (USERNAME CHAR(TEXT) ,
+    EMAIL TEXT  NOT NULL UNIQUE Primary Key,
+    CURRENT_BALANCE REAL); 
+EOF;
+ 
+$ret = pg_query($db, $sql);
+   pg_close($db); 
+include_once("Homepage.html");
 
 ?>
